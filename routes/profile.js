ab0ffffcb21b29fc57f6377c4d4a9865272eccba
traorefly <traorefly@tamu.edu>
2020-06-08 14:52:32 -0500

    milestone 2

--- routes/profile.js
+++ routes/profile.js
@@ -2,14 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 var profileData = require('../data/DALI_Data.json');
-var helpers = require ('../helpers/functions');
+var helpers = require('../helpers/functions');
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   var shuffledProfileData = helpers.shuffle(profileData);
-  var selectedProfileData = shuffledProfileData.slice(0,3);
-  res.render('profile/index',{profileData:profileData, selectedProfileData:selectedProfileData});
+  var selectedProfileData = shuffledProfileData.slice(0, 4);
+  var friends = shuffledProfileData.slice(4, 7);
+  var whoToFollow = shuffledProfileData.slice(7, 10);
+  res.render('profile/index', {
+    profileData: profileData,
+    selectedProfileData: selectedProfileData,
+    friends: friends,
+    whoToFollow: whoToFollow
+  });
 });
 
 module.exports = router;
