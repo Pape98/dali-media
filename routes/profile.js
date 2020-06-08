307c6c5153c38d60a89e657b36476a7c2efbb042
traorefly <traorefly@tamu.edu>
2020-06-08 09:55:03 -0500

    changed slider location

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var profileData = require('../data/DALI_Data.json');
+var helpers = require ('../helpers/functions');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('profile/index');
+  var shuffledProfileData = helpers.shuffle(profileData);
+  var selectedProfileData = shuffledProfileData.slice(0,3);
+  res.render('profile/index',{profileData:profileData, selectedProfileData:selectedProfileData});
 });
 
 module.exports = router;
