8c17b6e37295880126b547fe2a428f6b473cf04d
Pape98 <traorefly@tamu.edu>
2020-06-12 10:21:01 -0500

    allowed control origin 2

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
